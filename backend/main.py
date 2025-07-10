df5e2c3d105992a111fd5ce7ad8f5f260b6b9ca7
sunilravulapati <sunilravulapati028@gmail.com>
2025-07-10 20:28:49 +0530

    first done

--- backend/main.py
+++ backend/main.py
@@ -11,7 +11,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=["*"], # This allows all origins
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
